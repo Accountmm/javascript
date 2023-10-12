535a288c2ed8718a36a738f32d17ccacb952ed51
mmm <proweb2008.m@gmail.com>
2023-10-12 10:57:49 +0500

    mmm

---
+++ java.js
@@ -0,0 +1,43 @@
+let name =prompt ("Your name ?", "");
+alert("your name is " + name);
+
+let age = + prompt ("Your age?", "")
+alert ("your age is " +age)
+
+let plus = + prompt("1+1=?")
+alert ("your answer is " + plus)
+
+var  minus =  confirm ("5-2=3 ")
+alert (minus)
+
+var multiplication = + prompt ("2*4=?")
+alert ("your answer is " + multiplication)
+    
+let percent =  confirm ("3%2=1")
+alert ( percent)
+
+let divides = + prompt ("8/4=?")
+alert ("your answer is " + divides)
+
+var mmm = + prompt ("3+3=?")
+console.log(mmm);
+
+var plus2 = + prompt ("10+8=?")
+console.log(plus2);
+
+var minus2 =  confirm ("20-10=?")
+console.log(minus2);
+
+var minus3 =  confirm ("110-100=?")
+console.log(minus3);
+
+var percent2 = + prompt ("101%10=?")
+console.log(percent2);
+
+var multiplication2 = + prompt ("5*5+?")
+console.log(multiplication2);
+
+var multiplication3 = + prompt("1*11+?")
+console.log(multiplication3);
+
+alert ("Open console to show your 7 answers!!")
