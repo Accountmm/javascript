f16f10b958d6544ced92ac9505d4885d0bdda44f
mmm <proweb2008.m@gmail.com>
2023-10-13 17:02:51 +0500

    mam

--- java.js
+++ java.js
@@ -1,43 +1,109 @@
-let name =prompt ("Your name ?", "");
+let name = prompt("Your name ?", "");
 alert("your name is " + name);
+// if (name == String) {
+//     alert("your name is " + name);
+// } else {
+//     alert ("you type a number")
+// }
+let age = +prompt("Your age?", "")
+alert("your age is " + age)
 
-let age = + prompt ("Your age?", "")
-alert ("your age is " +age)
+let plus = +prompt("1+1=?")
+if (plus == 2) {
+    alert("Your answer is correct")
+} else if (plus > 2) {
+    alert("Your answer is not correct " + "Correct answer is 2")
+} else if (plus < 2) {
+    alert("Your answer is not correct " + "Correct answer is 2")
+} else {
+    alert("this is not a number pl type number")
+}
 
-let plus = + prompt("1+1=?")
-alert ("your answer is " + plus)
+// alert ("your answer is " + plus)
 
-var  minus =  confirm ("5-2=3 ")
-alert (minus)
+var minus = confirm("5-2=3 ")
+alert(minus)
 
-var multiplication = + prompt ("2*4=?")
-alert ("your answer is " + multiplication)
-    
-let percent =  confirm ("3%2=1")
-alert ( percent)
+var multiplication = +prompt("2*4=?")
+// alert ("your answer is " + multiplication)
+if (multiplication == 8) {
+    alert("your answer is correct")
+} else if (multiplication > 8 || multiplication < 8) {
+    alert("your answer is not corecct " + "corecct answer is 8")
 
-let divides = + prompt ("8/4=?")
-alert ("your answer is " + divides)
+} else {
+    alert("this is a word!!!!!!")
+}
 
-var mmm = + prompt ("3+3=?")
-console.log(mmm);
 
-var plus2 = + prompt ("10+8=?")
-console.log(plus2);
+let percent = confirm("3%2=1")
+alert(percent)
 
-var minus2 =  confirm ("20-10=?")
-console.log(minus2);
+let divides = +prompt("8/4=?")
+// alert ("your answer is " + divides)
+if (divides == 2) {
+    alert("your answer is correct")
+} else if (divides < 2 || divides > 2) {
+    alert("your answer is not correct " + "correct is 2")
+} else(
+    alert("this is not a number !!!!!!")
+)
 
-var minus3 =  confirm ("110-100=?")
-console.log(minus3);
+var mmm = +prompt("3+3=?")
+// console.log(mmm);
+if (mmm == 6) {
+    alert("your answer is correct")
+} else if (mmm > 6 || mmm < 6) {
+    alert("your answer is not correct " + "correct is 6")
 
-var percent2 = + prompt ("101%10=?")
-console.log(percent2);
+} else {
+    alert("this is not a number !!!!!!")
+}
 
-var multiplication2 = + prompt ("5*5+?")
-console.log(multiplication2);
+var plus2 = +prompt("10+8=?")
+// console.log(plus2);
+if (plus2 == 18) {
+    alert("your answer is correct")
+} else if (plus2 > 18 || plus2 < 18) {
+    alert("your answer is not correct " + "correct 18 ")
+} else {
+    alert("this is not a number !!!!!!")
+}
 
-var multiplication3 = + prompt("1*11+?")
-console.log(multiplication3);
+var minus2 = confirm("20-10=10")
+// console.log(minus2);
+alert(minus2)
+var minus3 = confirm("110-100=10")
+// console.log(minus3);
+alert(minus3)
+var percent2 = +prompt("101%10=?")
+// console.log(percent2);
+if (percent2 == 1) {
+    alert("your answer is correct")
+} else if (percent2 > 1 || percent2 < 1) {
+    alert("your answer is not correct " + "correct 18 ")
+} else {
+    alert("this is not a number !!!!!!")
+}
 
-alert ("Open console to show your 7 answers!!")
+var multiplication2 = +prompt("5*5+?")
+// console.log(multiplication2);
+if (multiplication2 == 25) {
+    alert("your answer is correct")
+} else if (multiplication2 > 25 || multiplication2 < 25) {
+    alert("your answer is not correct " + "correct 25 ")
+} else {
+    alert("this is not a number !!!!!!")
+}
+var multiplication3 = +prompt("1*11=?")
+// console.log(multiplication3);
+if (multiplication3 == 11) {
+    alert("your answer is correct")
+} else if (multiplication3 > 11 || multiplication3 < 11) {
+    alert("your answer is not correct " + "correct 11 ")
+}else {
+    alert ("type a number")
+}
+
+
+// alert ("Open console to show your 7 answmers!!")
